df090dba231782d16db8dbb1fefc5d7242b5fb82
Yan  Carlos Marín Osorio <yancarlosmarinosorio@gmail.com>
2019-10-31 11:53:23 -0500

    Updates

---
+++ precache-manifest.48f8adedf5557597d22f09c2888051d5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1edc817bfac4e502f48d5452e070168",
+    "url": "/cinne_frontend/index.html"
+  },
+  {
+    "revision": "1506aad439df539ee55d",
+    "url": "/cinne_frontend/static/css/2.a064d9b9.chunk.css"
+  },
+  {
+    "revision": "f445c0b93ec9e447ecd7",
+    "url": "/cinne_frontend/static/css/main.67a8fd4e.chunk.css"
+  },
+  {
+    "revision": "1506aad439df539ee55d",
+    "url": "/cinne_frontend/static/js/2.b167f008.chunk.js"
+  },
+  {
+    "revision": "f445c0b93ec9e447ecd7",
+    "url": "/cinne_frontend/static/js/main.e0095f4e.chunk.js"
+  },
+  {
+    "revision": "2677fe78c46a462f6cd4",
+    "url": "/cinne_frontend/static/js/runtime-main.ea9d1580.js"
+  },
+  {
+    "revision": "2e05eb8efcfae556b63e8ed866085b2b",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-100.2e05eb8e.woff2"
+  },
+  {
+    "revision": "5668013ade8bc06685304dc2f0ed0b52",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-100.5668013a.woff"
+  },
+  {
+    "revision": "0f09abbee49cd59e5f1f09351d6fbb1c",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-100italic.0f09abbe.woff2"
+  },
+  {
+    "revision": "b8cb8f05256f14abb872ab07142cfe5c",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-100italic.b8cb8f05.woff"
+  },
+  {
+    "revision": "085a034a09d96fba1b79a8cffb25855c",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-200.085a034a.woff2"
+  },
+  {
+    "revision": "d35ab26d6abb12514859fc18c7b0a4f4",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-200.d35ab26d.woff"
+  },
+  {
+    "revision": "3f941004b91782ab36c9aba71c75d13b",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-200italic.3f941004.woff"
+  },
+  {
+    "revision": "d507c473af2f2bbeb6257206894a5028",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-200italic.d507c473.woff2"
+  },
+  {
+    "revision": "23873196c5bdcf2aaf8fcaf9a8b8afb9",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-300.23873196.woff2"
+  },
+  {
+    "revision": "cb208c2654f45e37fea2f198ee508bdf",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-300.cb208c26.woff"
+  },
+  {
+    "revision": "0a034998d85215274a280f0cbe538814",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-300italic.0a034998.woff2"
+  },
+  {
+    "revision": "8191cc0be7cc83a5be922f3f9d76b4cb",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-300italic.8191cc0b.woff"
+  },
+  {
+    "revision": "0c995470f5c1ee441ee856149494b301",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-400.0c995470.woff2"
+  },
+  {
+    "revision": "14f9c88b615440e76c2ab3cceb340728",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-400.14f9c88b.woff"
+  },
+  {
+    "revision": "05827e5cd43063a61fdfab5b449cecb1",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-400italic.05827e5c.woff"
+  },
+  {
+    "revision": "2a4c48dbd6e502cb43ff104c526e7231",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-400italic.2a4c48db.woff2"
+  },
+  {
+    "revision": "2b8c1473f2b21fe9054b076fed888501",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-500.2b8c1473.woff"
+  },
+  {
+    "revision": "464d2ccbb0516f97b96846accd2da1c1",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-500.464d2ccb.woff2"
+  },
+  {
+    "revision": "8c5505c9660eb88e28feba1dcb22014f",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-500italic.8c5505c9.woff"
+  },
+  {
+    "revision": "b84c4f1b90d78567c828685ee3d2c7c4",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-500italic.b84c4f1b.woff2"
+  },
+  {
+    "revision": "0d35a4cff9f031781df02a86745a7df0",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-600.0d35a4cf.woff2"
+  },
+  {
+    "revision": "90e2cfd631894599b13c885abca1ccb3",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-600.90e2cfd6.woff"
+  },
+  {
+    "revision": "4e52a42d672bab1a2e39c93b5ce5f22d",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-600italic.4e52a42d.woff2"
+  },
+  {
+    "revision": "7e2f06d65cc203f5baa971b59d12d179",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-600italic.7e2f06d6.woff"
+  },
+  {
+    "revision": "bb5960211a6908dc18ecdafe82b16765",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-700.bb596021.woff"
+  },
+  {
+    "revision": "ea59e2fc1e0ed6276f905e641e095112",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-700.ea59e2fc.woff2"
+  },
+  {
+    "revision": "977866f3d7ef08fd8564fdc2315d34f1",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-700italic.977866f3.woff2"
+  },
+  {
+    "revision": "dd03d8e092413749806e35ec1434a4b6",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-700italic.dd03d8e0.woff"
+  },
+  {
+    "revision": "540f244cbb7984abe500b08a7a4146d3",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-800.540f244c.woff"
+  },
+  {
+    "revision": "6a69d9fc4a868e6c048747809cd37ae0",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-800.6a69d9fc.woff2"
+  },
+  {
+    "revision": "16ad7ecd4848ec27afa5f2915c2f95c1",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-800italic.16ad7ecd.woff"
+  },
+  {
+    "revision": "99486d94bc18f3b8785d08d7f601e2b6",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-800italic.99486d94.woff2"
+  },
+  {
+    "revision": "a3437410b2ba2e3519b9308b92219231",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-900.a3437410.woff"
+  },
+  {
+    "revision": "bc38c6edf6274fc5259670ca78411c86",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-900.bc38c6ed.woff2"
+  },
+  {
+    "revision": "4c606015c9cb5c83275f8c714f993586",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-900italic.4c606015.woff"
+  },
+  {
+    "revision": "816df906a38e4c45e5c165e106fb1953",
+    "url": "/cinne_frontend/static/media/montserrat-alternates-latin-900italic.816df906.woff2"
+  }
+]);
